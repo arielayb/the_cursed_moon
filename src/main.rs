41ac82b419ccfffa81c9f8e3ff7bf81495284fa7
arielayb <cyberspace009@bitbucket.org>
2023-12-29 18:48:30 -0700

    added Bevy deps

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-use rltk::{Rltk, GameState};
+use bevy::prelude::*;
 
-struct State {}
-impl GameState for State {
-    fn tick(&mut self, ctx : &mut Rltk) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
-    }
+#[derive(Component)]
+struct Person;
+
+fn hello_world(){
+    println!("The cursed moon");
 }
 
-fn main() -> rltk::BError {
-    use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
-        .with_title("Roguelike Tutorial")
-        .build()?;
-    let gs = State{ };
-    rltk::main_loop(context, gs)
+fn main() {
+    App::new().add_systems(Update, hello_world).run();
 }
