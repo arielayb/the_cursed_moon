f3a50541379088fe0d01c59af96f928f5840d224
arielayb <cyberspace009@bitbucket.org>
2023-12-30 17:25:40 -0700

    completed bevy tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use bevy::prelude::*;
 
+
+pub struct HelloPlugin;
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        // add things to your app here
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+           .add_systems(Startup, add_people)
+           .add_systems(Update, greet_people);
+    }
+}
+
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
 
-fn hello_world(){
-    println!("The cursed moon");
-}
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
@@ -16,6 +26,14 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
 fn main() {
-    App::new().add_systems(Startup, add_people).add_systems(Update, hello_world).run();
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
 }
